6552b66a3f47c0f4c699cc5071277d4abd113d37
Rachel Young <reyoung255@gmail.com>
2023-02-03 10:35:20 +0100

    Event component

---
+++ src/js/Event.js
@@ -0,0 +1,33 @@
+export default class Event {
+    constructor(properties) {
+        this.name = properties.name;
+        this.date = properties.date;
+        this.description = properties.description;
+        this.imageUrl = properties.image_url;
+        this.element = document.createElement('div');
+        this.element.classList.add("event");
+
+        this.createEvent();
+    }
+
+    createEvent = () => {
+        this.element.innerHTML = `
+       
+        <img
+          class="event-img"
+          src="${this.imageUrl}"
+          alt=""
+        />
+        <div class="event-info">
+          <h2 class="event-name">
+            ${this.name}
+          </h2>
+          <p class="event-date">${this.date}</p>
+          <button class="more-btn">More</button>
+        </div>
+        `
+        const main = document.querySelector('main');
+        main.appendChild(this.element)
+    }
+
+}
---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+import Event from './Event';
+
+const event1 = new Event({
+    "id": 1,
+    "name": "PRAGUE CARNIVAL (MASOPUST) - FESTIVAL & PARADE",
+    "date": "2022-02-26",
+    "description": "Prague Carnival (Masopust) traces its roots back to medieval times, and is a celebration of human joy and gregariousness. Set hours for the various events are not generally adhered to, everything is far too free spirited for that! Just spend time in the Lesser Town and if you see colourfully dressed people and hear loud music, you know the carnival has arrived. The Masks Parade is the most celebrated element of Prague Carnival. It starts at U Černého vola pub at Loretanske namesti near Prague Castle, winds down through the Lesser Town, and should arrive at Kampa Island around 3-4pm, where a party is held with live music and festival food and drinks on sale.",
+    "image_url": "http://www.progetto.cz/wp-content/uploads/2015/10/33-Masopust.jpg"
+  })
+
+  const event2 = new Event({
+    "id": 2,
+    "name": "ST MATTHEW'S FAIR",
+    "date": "2022-02-26",
+    "description": "Prague Carnival (Masopust) traces its roots back to medieval times, and is a celebration of human joy and gregariousness. Set hours for the various events are not generally adhered to, everything is far too free spirited for that! Just spend time in the Lesser Town and if you see colourfully dressed people and hear loud music, you know the carnival has arrived. The Masks Parade is the most celebrated element of Prague Carnival. It starts at U Černého vola pub at Loretanske namesti near Prague Castle, winds down through the Lesser Town, and should arrive at Kampa Island around 3-4pm, where a party is held with live music and festival food and drinks on sale.",
+    "image_url": "https://www.praguest.com/images/sampledata/sports/iceskating/ovocnytrh/estate-theatre.jpg"
+  })
+
+  const event3 = new Event({
+    "id": 3,
+    "name": "PRAGUE CARNIVAL (MASOPUST) - FESTIVAL & PARADE",
+    "date": "2022-02-26",
+    "description": "Prague Carnival (Masopust) traces its roots back to medieval times, and is a celebration of human joy and gregariousness. Set hours for the various events are not generally adhered to, everything is far too free spirited for that! Just spend time in the Lesser Town and if you see colourfully dressed people and hear loud music, you know the carnival has arrived. The Masks Parade is the most celebrated element of Prague Carnival. It starts at U Černého vola pub at Loretanske namesti near Prague Castle, winds down through the Lesser Town, and should arrive at Kampa Island around 3-4pm, where a party is held with live music and festival food and drinks on sale.",
+    "image_url": "https://www.welcometoprague.eu/storage/upload/images/Entertainmet/Fair_large.jpg"
+  })
+
