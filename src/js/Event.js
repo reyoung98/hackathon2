502097e0f80fbadca94906d8ab021ff698ff2a3c
Alena Kellnerova <kellnerovaajka@gmail.com>
2023-02-08 17:23:49 +0100

    final touch on form, get rid of last for loop

--- src/js/Event.js
+++ src/js/Event.js
@@ -35,21 +35,21 @@ export default class Event {
           method="POST"
           action="https://test-api.codingbootcamp.cz/api/532f6503/events/${this.id}/registrations"
         >
-          <label for="first_name">Name*</label> <br />
+          <label for="first_name">Name *</label> <br />
           <input type="text" name="first_name" id="first_name" /> <br />
 
-          <label for="last_name">Surname*</label> <br />
+          <label for="last_name">Surname *</label> <br />
           <input type="text" name="last_name" id="last_name" />
 
-          <label for="email">Email*</label> <br />
-          <br />
+          <label for="email">Email *</label> <br />
+        
           <input type="email" name="email" id="email"/> <br />
 
-          <label for="phone">Phone Number:</label> <br />
+          <label for="phone">Phone Number</label> <br />
           <input type="tel" name="phone" id="phone"/> <br />
 
           <div class="check">
-          <label for="age_check">I am over 18 </label>
+          <label for="age_check">I am over 18 *</label>
           <input type="checkbox" name="age_check" id="age_check"/>
           </div>
           <button type="submit" >Send</button>
@@ -61,16 +61,22 @@ export default class Event {
         </div>
        
         `;
+    // Append Modal inside the section
 
     const section = document.querySelector("section");
     section.appendChild(this.modal);
+
+    // Select Close Button and add event listener
 
     const xBtn = this.modal.querySelector(".x-btn");
     console.log(xBtn);
     xBtn.addEventListener("click", this.closeModal);
 
+    //  Select the Form and add event listener type Submit to it
+    // Prevent default, trigger sendData(form) function with form as an argument
+
     const form = this.modal.querySelector("form");
-    console.log(form);
+    // console.log(form);
     form.addEventListener("submit", (e) => {
       e.preventDefault();
       this.sendData(form);
@@ -99,16 +105,24 @@ export default class Event {
   };
 
   openModal = () => {
-    const moreBtns = document.querySelectorAll(".more-btn");
+    // const moreBtns = document.querySelectorAll(".more-btn");
 
-    for (let i = 0; i < moreBtns.length; i++) {
-      moreBtns[i].addEventListener("click", () => {
-        if (i + 7 === this.id) {
-          this.modal.classList.add("modal-visible");
-          this.createModal();
-        }
-      });
-    }
+    // for (let i = 0; i < moreBtns.length; i++) {
+    //   moreBtns[i].addEventListener("click", () => {
+    //     if (i + 7 === this.id) {
+    //       this.modal.classList.add("modal-visible");
+    //       this.createModal();
+    //     }
+    //   });
+    // }
+
+    //
+
+    const moreBtn = this.event.querySelector(".more-btn");
+    moreBtn.addEventListener("click", () => {
+      this.modal.classList.add("modal-visible");
+      this.createModal();
+    });
   };
 
   closeModal = () => {
@@ -124,7 +138,6 @@ export default class Event {
     const lastName = form.querySelector("#last_name").value;
     const emailValue = form.querySelector("#email").value;
     const phoneValue = form.querySelector("#phone").value;
-    // const lastName = form.querySelector('#last_name').value;
 
     const url = `https://test-api.codingbootcamp.cz/api/532f6503/events/${this.id}/registrations`;
     const dataObject = {
